13bbc40ff5ae0ac0f6b9a0017d06d8460ed342da
Philip Tzou <philip.npc@gmail.com>
2020-07-28 09:25:34 -0700

    Add mutation annotations for Spike gene

---
+++ scripts/import_annotations.py
@@ -0,0 +1,157 @@
+#! /usr/bin/env python3
+
+import sys
+import csv
+import json
+
+
+def load_config(config_file):
+    config = json.load(config_file)
+    citations = {}
+    reverse_citations = {}
+    cite_autoincur = 1
+    for key, attrs in config['annotations'].items():
+        if key.startswith('#'):
+            continue
+        for citation in attrs['citations']:
+            author = citation['author']
+            year = citation['year']
+            doi = citation['doi']
+            section = citation.get('section', '__default__')
+            if doi not in reverse_citations:
+                reverse_citations[doi] = {
+                    'idx': cite_autoincur,
+                    'author': author,
+                    'year': year,
+                    'sections': {}
+                }
+                cite_autoincur += 1
+            r_citation = reverse_citations[doi]
+            if section not in r_citation['sections']:
+                sections = r_citation['sections']
+                if sections:
+                    sections[section] = max(sections.values()) + 1
+                else:
+                    sections[section] = 1
+                citations['{}.{}'.format(
+                    r_citation['idx'],
+                    sections[section]
+                )] = {
+                    'citation_id': r_citation['idx'],
+                    'section_id': sections[section],
+                    **citation
+                }
+    config['citations'] = citations
+    config['reverse_citations'] = reverse_citations
+    return config
+
+
+def get_citation_ids(attrs, config):
+    all_citations = config['reverse_citations']
+    citation_ids = []
+    for c in attrs['citations']:
+        citation = all_citations[c['doi']]
+        section_id = citation['sections'][c['section']]
+        idx = (citation['idx'], section_id)
+        citation_ids.append(idx)
+    return set(citation_ids)
+
+
+def reform_posdata(posdata):
+    posdata['annotations'] = [{
+        **annotation,
+        'citation_ids': [
+            '{}.{}'.format(*idx)
+            for idx in sorted(annotation['citation_ids'])
+        ]
+    } for annotation in posdata['annotations'].values()]
+    posdata['aminoAcids'] = sorted(
+        ({
+            **aadata,
+            'annotations': [{
+                **annotation,
+                'citation_ids': [
+                    '{}.{}'.format(*idx)
+                    for idx in sorted(annotation['citation_ids'])
+                ]
+            } for annotation in aadata['annotations'].values()]
+        } for aadata in posdata['aminoAcids'].values()),
+        key=lambda d: d['aminoAcid']
+    )
+    if not posdata['annotations'] and not posdata['aminoAcids']:
+        return
+    return posdata
+
+
+def main():
+    if len(sys.argv) != 4:
+        print('Usage: {} <CONFIG_PATH> <INPUT_CSV> <OUTPUT_JSON>'
+              .format(sys.argv[0]), file=sys.stderr)
+        exit(1)
+    config, infile, outfile = sys.argv[1:]
+    with open(config, 'r', encoding='utf-8-sig') as config, \
+            open(infile, 'r', encoding='utf-8-sig') as infile, \
+            open(outfile, 'w') as outfile:
+        config = load_config(config)
+        rows = csv.DictReader(infile)
+        all_posdata = []
+        for row in rows:
+            pos = row['position']
+            posdata = {
+                'position': pos,
+                'annotations': {},
+                'aminoAcids': {}
+            }
+            for key, attrs in config['annotations'].items():
+                if key.startswith('#'):
+                    continue
+                name = attrs['name']
+                value = row[key].strip()
+                if not value:
+                    continue
+                level = attrs['level']
+                citation_ids = get_citation_ids(attrs, config)
+                if level == 'position':
+                    annotations = posdata['annotations']
+                    annotations.setdefault(name, {
+                        'name': name,
+                        'value': value,
+                        'citation_ids': set()
+                    })['citation_ids'] |= citation_ids
+                elif level == 'amino acid':
+                    aadata = posdata['aminoAcids']
+                    value = (
+                        value
+                        .replace('ins', 'i')
+                        .replace('del', 'd')
+                        .replace('unseq', '?')
+                        .replace('Unsequenced', '?')
+                    )
+                    for aa in value:
+                        aa = aa.strip()
+                        if not aa or aa == '?':
+                            continue
+                        annotations = (
+                            aadata
+                            .setdefault(aa, {
+                                'aminoAcid': aa,
+                                'annotations': {}
+                            })['annotations']
+                        )
+                        annotations.setdefault(name, {
+                            'name': name,
+                            'citation_ids': set()
+                        })['citation_ids'] |= citation_ids
+            posdata = reform_posdata(posdata)
+            if posdata:
+                all_posdata.append(posdata)
+        json.dump({
+            'gene': config['gene'],
+            'taxonomy': config['taxonomy'],
+            'positions': sorted(all_posdata, key=lambda d: d['position']),
+            'citations': config['citations']
+        }, outfile, indent=2)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/import_sars2_spike_annotations.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -e
+
+cd $(dirname $0)/..
+
+BOLD=$(tput bold)
+NORM=$(tput sgr0)
+
+if ! which ssconvert 1>/dev/null 2>/dev/null ; then
+    echo "Abort: unable to find command ${BOLD}ssconvert${NORM} from package ${BOLD}gnumeric${NORM}." 1>&2
+    exit 1
+fi
+
+XLSX_PATH="$HOME/dropbox/Coronavirus/Analyses/SpikeAnnotationRS.xlsx"
+CSV_PATH=$(mktemp)
+
+trap "rm $CSV_PATH" EXIT
+
+ssconvert $XLSX_PATH $CSV_PATH --export-type="Gnumeric_stf:stf_csv"
+scripts/import_annotations.py scripts/sars2_spike_annotations.json $CSV_PATH src/main/resources/mutation-annotations_S.json
