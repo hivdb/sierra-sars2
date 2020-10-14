cb6821dfad1685ee5f6005f5489a037809bca9e2
Philip Tzou <philip.npc@gmail.com>
2020-10-14 14:28:16 -0700

    Clean up

--- scripts/update_annotations.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -e
-
-cd $(dirname $0)/..
-
-./scripts/update_annotations.py scripts/update_annotations_config.json
-./scripts/clean_annotations.py src/main/resources/mutation-annotations_SARS2S.json
